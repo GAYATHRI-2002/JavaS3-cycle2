e6f50df882d5a6dd759befb23d0b7e869c23a2cc
GAYATHRI-2002 <83536175+GAYATHRI-2002@users.noreply.github.com>
2022-01-24 21:39:22 +0530

    Create scharacter.java

---
+++ scharacter.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class scharacter
+{
+
+  public static void main(String args[])
+  { 
+     int i,flag=0;
+    Scanner obj=new Scanner (System.in);
+    System.out.println("enter the string length");
+    int n= obj.nextInt();
+    System .out .printlmn("enter the string");
+    char a[]=newchar[20];
+    for(i=0;i<n;i++)
+      a[i]=obj.next()charAt(0);
+    System.out.println("enter the character to be searched");
+    char ch=obj.next()charAt[0]:
+      for(i=0;i<n;i++)
+      {
+         if (a[i]==ch)
+         {
+            flag=0;
+           break;
+         }
+        else
+          flag=1;
+      }
+    
